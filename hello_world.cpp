c113a334fe4ac484e05fc7cc3c766d2304483081
R2D2CST <r2d2cst@gmail.com>
2024-05-22 15:07:16 -0600

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+    // Comentario : Función que escribe hola mundo
+    cout << "Hola mundo" " curso Yacklion";
+    // Puedo declarar mas de un mensaje en el mismo renglón.
+    return 0;
+}
